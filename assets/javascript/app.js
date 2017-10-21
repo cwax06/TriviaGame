4ab3e34f1ad94e3ae18b05753f1e436cbdd89100
Andrew Jones <Andrew@AHJones-iMac.local>
2017-10-21 17:07:57 -0600

    questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+// Variables
+//=====================================================
+
+var $questionContent = $("<div></div>");
+
+const myQuestions = [
+    {
+        question: "What is Jerry's Address?"
+        answers: {
+            a: "121 West 81st Street",
+            b: "121 East 81st Street",
+            c: "221 West 31st Street",
+            d: "331 East 31st Street"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the name of Kramer's freind that is never seen?"
+        answers: {
+            a: "Joe Divola",
+            b: "Bob Sacamano",
+            c: "Micky Taylor",
+            d: "Bob Cobb"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Who is the co-creator of Seinfeld?"
+        answers: {
+            a: "Jason Alexander",
+            b: "Elaine Benese",
+            c: "Larry David",
+            d: "Frank Constanza"
+        },
+        correctAnswer: "c"
+    },
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functions
+//=====================================================
+
+
+for (vari = 0; i < questionArray.length; ++i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Instructions
+//=====================================================
